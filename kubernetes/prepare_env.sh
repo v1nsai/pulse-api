338a03a7b3939d8ca86a8772b860c8d41cc869d8
doctor_ew <doctor_ew@protonmail.com>
2025-08-18 13:40:05 -0400

    deployment config

--- kubernetes/prepare_env.sh
+++ kubernetes/prepare_env.sh
@@ -8,11 +8,20 @@ if [[ -z "${HARBOR_SERVER}" || -z "${HARBOR_USERNAME}" || -z "${HARBOR_PASSWORD}
   exit 1
 fi
 
+# Create namespace and service account for jenkins to use
 kubectl apply -f ./kubernetes/namespace.yaml 
 kubectl apply -f ./kubernetes/serviceaccount.yaml
+
+# Harbor registry URL and secret
+kubectl delete secret harbor-registry-secret --namespace pulse || true
 kubectl create secret docker-registry harbor-registry-secret \
   --docker-server=${HARBOR_SERVER} \
   --docker-username=${HARBOR_USERNAME} \
   --docker-password=${HARBOR_PASSWORD} \
   --docker-email=${HARBOR_EMAIL} \
   --namespace pulse
+
+# add harbor to talos cluster
+source kubernetes/.env
+envsubst < ./kubernetes/registry_patch.template.yaml > /tmp/registry_patch.yaml
+talosctlwrapper all patch machineconfig --patch @/tmp/registry_patch.yaml
